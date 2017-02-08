75b1d854f23766fe7f38f72bf6635937e2565023
AMER\BXD6826 <benjamin_daniel@homedepot.com>
2017-02-07 20:31:27 -0500

    data is plotting now there is a delay when working with plotly.

--- src/starbucks_data_visualization_analysis.R
+++ src/starbucks_data_visualization_analysis.R
@@ -4,8 +4,8 @@ library(tidyr)
 library(reshape2)
 library(dplyr)
 #boxplot the total income
-p<-plot_ly(df3,y=~total_income,x=~has_starbucks,  type="box")
-p
+#p<-plot_ly(df3,y=~total_income,x=~has_starbucks,  type="box")
+#p
 
 # 1 = $1 under $25,000
 # 2 = $25,000 under $50,000
@@ -14,7 +14,7 @@ p
 # 5 = $100,000 under $200,000
 # 6 = $200,000 or more
 label = c("agi_one","agi_two","agi_three","agi_four","agi_five","agi_six")
-bucket = c("$1 under $25,000","$25,000 under $50,000","$50,000 under $75,000","$75,000 under $100,000","$100,000 under $200,000","$200,000 or more")
+bucket = c("1 under 25,000","25,000 under 50,000","50,000 under 75,000","75,000 under 100,000","100,000 under 200,000","200,000 or more")
 position = c(1,2,3,4,5,6)
 dflegend<-data.frame(label,bucket,position,stringsAsFactors = FALSE)
 
@@ -27,11 +27,14 @@ dfmelt<-  inner_join(dfmelt, dflegend,by=c("variable" = "label"))%>%
 arrange(position)%>%
 #number of returns in each bucket
 #dfmelt<-melt(df3,measure.vars=15:20)%>%filter(YEAR==2014)%>%
-  select(one_of(c("variable", "value","has_starbucks")))
+select(one_of(c("variable", "value","has_starbucks","bucket")))
 
-ggplot(dfmelt,  aes(x=has_starbucks, y=value,fill=variable))+
+dfmelt$bucketf = factor(dfmelt$bucket,levels=c("75,000 under 100,000","100,000 under 200,000","200,000 or more"))
+
+ggplot(dfmelt,  aes(x=has_starbucks, y=value,fill=bucket))+
   geom_boxplot()+
-  facet_grid(.~variable)
+  #facet_grid(.~bucket)
+  facet_grid(.~bucketf)
 ggplotly()
 
 
